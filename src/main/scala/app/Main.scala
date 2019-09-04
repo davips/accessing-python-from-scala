86188a25420af924338428b9da84f7307c9f17ec
davips <dpsabc@gmail.com>
2019-09-04 18:35:16 -0300

    DT

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,13 +1,24 @@
 package app
 
+import java.util
+
 import jep.Jep
+import jep.python.PyObject
 
 object Main extends App {
   val jep = new Jep()
-  jep.eval("s = 'Hello World'")
-  jep.eval("print(s)")
-  val java_string = jep.getValue("s").toString
-  println("Java String:" + java_string)
+
+  def comp(module: String, name: String) = {
+    import scala.jdk.CollectionConverters._
+    jep.eval(s"from $module import $name")
+    jep.getValue("DT.cs().serialize()")
+  }
+
+  val dt = comp("paje.ml.element.modelling.supervised.classifier.dt", "DT")
+  println("[[" + dt + "]]")
+//  println("[[" + dt("criterion")("a") + "]]")
+  println("[[" + dt.getClass + "]]")
+
   jep.runScript("src/main/python/demo.py")
   print(jep.getValue("asd"))
 }
